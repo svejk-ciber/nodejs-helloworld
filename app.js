9e053ccdf31f1743323501e91f0ef640f4c6a37e
Sven-Jørgen Karlsen <svejk@hotmail.no>
2020-03-06 00:39:23 +0100

    Add mandatory env. var for configuration debugging scenarios.

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
 
+if (! ("WORLD_STATE" in process.env)) {
+  throw "WORLD_STATE is not set in the process environment.";  
+}
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.end(`Goodbye, ${process.env.WORLD_STATE} world!`);
 });
 
 server.listen(port, hostname, () => {
